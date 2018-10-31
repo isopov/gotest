14166737fd6eb54a344b2f436e5dfba6a2d807b1
Ivan Sopov <sopov.ivan@gmail.com>
2018-10-31 14:45:43 +0300

    errors sample (with errorx)

---
+++ errors/errors_test.go
@@ -0,0 +1,38 @@
+package errors
+
+import (
+	"fmt"
+	"github.com/joomcode/errorx"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"testing"
+)
+
+func Foo() *errorx.Error {
+	return errorx.IllegalState.New("foobar")
+}
+
+func Bar() *errorx.Error {
+	err := Foo()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Baz() *errorx.Error {
+	err := Bar()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func TestErrors(t *testing.T) {
+	err := Baz()
+	log.Printf()
+	errorMsg := fmt.Sprintf("Error: %+v", err)
+	assert.Contains(t, errorMsg, "errors.Foo")
+	assert.Contains(t, errorMsg, "errors.Bar")
+	assert.Contains(t, errorMsg, "errors.Baz")
+}
